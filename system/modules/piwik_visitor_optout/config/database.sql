46769788178d9af9ba91a1c0c99cfe91ad6f19d7
Nils <nils@poltmann.com>
2013-02-08 10:31:26 +0100

    Init Commit

---
+++ system/modules/piwik_visitor_optout/config/database.sql
@@ -0,0 +1,9 @@
+-- --------------------------------------------------------
+ 
+-- 
+-- Table `tl_module`
+-- 
+ 
+CREATE TABLE `tl_module` (
+  `piwik_visitor_optout_url` varchar(255) NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ system/modules/piwik_visitor_optout/contao_piwik_visitor_optout.php
@@ -0,0 +1,51 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao-Piwik-Visitor-OptOut
+ * Copyright (C) 2013 Nils Poltmann
+ *
+ * Extension for:
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Nils Poltmann
+ * @author     Nils Poltmann
+ * @package    Contao-Piwik-Visitor-OptOut
+ */
+ 
+class contao_piwik_visitor_optout extends Module {
+
+	protected $strTemplate = 'contao_piwik_visitor_optout'; 
+
+	public function generate() {
+		if(TL_MODE == 'BE') {
+			$t = new BackendTemplate('be_wildcard');
+			$t->wildcard = '### Contao Piwik Visitor Optout ###';
+			return($t->parse());
+		}
+		
+		return parent::generate();
+	}
+	
+	public function compile(){
+		
+	}
+	
+}
